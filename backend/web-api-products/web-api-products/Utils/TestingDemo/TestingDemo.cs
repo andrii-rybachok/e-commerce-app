2b2a7285ce7e544ba8258038dfb284767dc4bf1c
Andriuha <a.rybachok90@gmail.com>
2023-03-31 18:36:29 +0200

    add some utils for convinient using filtering and other things

---
+++ backend/web-api-products/web-api-products/Utils/TestingDemo/TestingDemo.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Reflection.Metadata;
+using web_api_products.DataBase;
+using web_api_products.Models;
+using web_api_products.Models.Gadgets;
+
+namespace web_api_products.Utils.TesingDemo
+{
+	[TestClass]
+	public class NonQueryTests
+	{
+		[TestMethod]
+		public void CreateProduct_saves_a_product_via_context()
+		{
+			var mockSet = new Mock<DbSet<Product>>();
+
+			var mockContext=new Mock<ShopDB>();
+			mockContext.Setup(m=>m.Products).Returns(mockSet.Object);
+
+			var service = new ProductService(mockContext.Object);
+			service.AddProduct(new Headphone { Name = "SoundBlast", Description = "Premium noise-canceling headphones", Price = 299, Producer = new Producer() { Name="Bose"}, HeadphoneType =HeadphoneType.Wireless });
+
+			mockSet.Verify(m => m.Add(It.IsAny<Product>()), Times.Once());
+			mockContext.Verify(m => m.SaveChanges(), Times.Once());
+			
+		}
+	}
+}
