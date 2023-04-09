16163fa24aece11bef92bbad16cc0408c9bb39c7
Andriuha <a.rybachok90@gmail.com>
2023-04-09 16:45:37 +0200

    added news

--- backend/web-api-products/web-api-products/Utils/AutoMapper.cs
+++ backend/web-api-products/web-api-products/Utils/AutoMapper.cs
@@ -5,38 +5,48 @@ using web_api_products.Models.DTO.Products;
 
 namespace web_api_products.Utils
 {
-	public static class Mappers
+	//public static class Mappers
+	//{
+	//	private static string[] getImages(IEnumerable<Image> images)
+	//	{
+	//		string[] names = new string[images.Count()];
+	//		for (int i = 0; i < names.Count(); i++)
+	//		{
+	//			names[i] = images.ElementAt(i).Name;
+	//		}
+	//		return names;
+	//	}
+	//	public static Mapper ProductDTOMapper()
+	//	{
+
+	//		var config = new MapperConfiguration(cfg =>
+	//		{
+	//			cfg.CreateMap<Product, ProductDTO>()
+	//			.ForMember(x => x.PreviewImage, act =>act.MapFrom(src=>src.ProductImages.FirstOrDefault().Name))
+	//			.ForMember(x => x.SecondImage, act => act.MapFrom(src => src.ProductImages.ElementAtOrDefault(1).Name));
+	//		});
+	//		var mapper = new Mapper(config);
+	//		return mapper;
+	//	}
+	//	public static Mapper PopularCategoryDTOMapper()
+	//	{
+
+	//		var config = new MapperConfiguration(cfg =>
+	//		{
+	//			cfg.CreateMap<Category, PopularCategoryDTO>();
+	//		});
+	//		var mapper = new Mapper(config);
+	//		return mapper;
+	//	}
+	//}
+	public class MapperConfig : Profile
 	{
-		private static string[] getImages(IEnumerable<Image> images)
+		public MapperConfig()
 		{
-			string[] names = new string[images.Count()];
-			for (int i = 0; i < names.Count(); i++)
-			{
-				names[i] = images.ElementAt(i).Name;
-			}
-			return names;
-		}
-		public static Mapper ProductDTOMapper()
-		{
-
-			var config = new MapperConfiguration(cfg =>
-			{
-				cfg.CreateMap<Product, ProductDTO>()
-				.ForMember(x => x.PreviewImage, act =>act.MapFrom(src=>src.ProductImages.FirstOrDefault().Name))
+			CreateMap<Category, PopularCategoryDTO>();
+			CreateMap<Product, ProductDTO>()
+				.ForMember(x => x.PreviewImage, act => act.MapFrom(src => src.ProductImages.FirstOrDefault().Name))
 				.ForMember(x => x.SecondImage, act => act.MapFrom(src => src.ProductImages.ElementAtOrDefault(1).Name));
-			});
-			var mapper = new Mapper(config);
-			return mapper;
-		}
-		public static Mapper PopularCategoryDTOMapper()
-		{
-
-			var config = new MapperConfiguration(cfg =>
-			{
-				cfg.CreateMap<Category, PopularCategoryDTO>();
-			});
-			var mapper = new Mapper(config);
-			return mapper;
 		}
 	}
 }
